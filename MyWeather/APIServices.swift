9fa204322b66f57a4af324e4c7061a48e9f78f3c
DenAlNik <sasha.russia555@mail.ru>
2024-04-11 12:22:05 +0300

    card of weather on main screen

--- MyWeather/APIServices.swift
+++ MyWeather/APIServices.swift
@@ -4,5 +4,21 @@
 //
 //  Created by Александр Денисов on 11.04.2024.
 //
+//e721d68acfa85590809be07883665271
 
 import Foundation
+
+struct StateOfWeather {
+    let city: String
+    let temp: String
+    let typeOfWeather: String
+    let windSpeed: String
+    let windSide: String
+  }
+
+
+func fetchWeather(completionHandler: @escaping([StateOfWeather]) -> Void) {
+    var weather: [StateOfWeather] = []
+    
+    completionHandler(weather)
+}
--- MyWeather/MainViewController.swift
+++ MyWeather/MainViewController.swift
@@ -9,7 +9,14 @@ import UIKit
 import PinLayout
 
 class MainViewController: UIViewController {
-
+    private let currentCityWeather: StateOfWeather = StateOfWeather(city: "Moscow", temp: "+15", typeOfWeather: "sun", windSpeed: "10", windSide: "south")
+    private let cityLabel = UILabel()
+    private let tempLabel = UILabel()
+    private let windSpeedLabel = UILabel()
+    private let windSideLabel = UILabel()
+    private let typeOfWeatherLabel = UILabel()
+    private let cardView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -19,13 +26,74 @@ class MainViewController: UIViewController {
     private func setup() {
         view.backgroundColor = .systemGray6
         title = tabBarItem.title
+        //navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "slider.horizontal.2.square"),
+            style: .plain,
+            target: self,
+            action: #selector(didSettingsButtonTapped))
         
-
+        
+        
+        [cardView].forEach {
+            view.addSubview($0)
+        }
+        [cityLabel, tempLabel, windSpeedLabel, windSideLabel, typeOfWeatherLabel].forEach {
+            cardView.addSubview($0)
+        }
+        cardView.backgroundColor = .white
+        cardView.frame = CGRect(x: 0, y: 0, width: Int(self.view.safeAreaLayoutGuide.layoutFrame.width), height: 168)
+        cardView.layer.cornerRadius = 8
+        setCurrent()
+        cityLabel.font = UIFont.systemFont(ofSize: 30, weight: .semibold)
+        
+       
     }
+    
+    private func setCurrent() {
+        self.cityLabel.text = currentCityWeather.city
+        self.tempLabel.text = "Температура: " + currentCityWeather.temp + " C˚"
+        self.windSpeedLabel.text = "Скорость ветра: " + currentCityWeather.windSpeed + " м/с"
+        self.windSideLabel.text = "Направление ветра: " + currentCityWeather.windSide
+        self.typeOfWeatherLabel.text = "Состояние погоды: " + currentCityWeather.typeOfWeather
+    }
+    
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
+        cardView.pin
+            .top(view.pin.safeArea.top + 12)
+            .horizontally(view.pin.safeArea + 20)
+        cityLabel.pin
+            .top(12)
+            .left(20)
+            .sizeToFit()
+        typeOfWeatherLabel.pin
+            .below(of: cityLabel)
+            .marginTop(8)
+            .left(20)
+            .sizeToFit()
+        tempLabel.pin
+            .below(of: typeOfWeatherLabel)
+            .marginTop(4)
+            .left(20)
+            .sizeToFit()
+        windSpeedLabel.pin
+            .below(of: tempLabel)
+            .marginTop(4)
+            .left(20)
+            .sizeToFit()
+        windSideLabel.pin
+            .below(of: windSpeedLabel)
+            .marginTop(4)
+            .left(20)
+            .sizeToFit()
+    }
+    
+    @objc
+    private func didSettingsButtonTapped() {
+        
     }
 }
 
